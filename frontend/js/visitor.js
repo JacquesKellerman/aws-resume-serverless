2d33d0d783fd44ce257a7947bf121f55a19dfef3
Jacques Kellerman <jacquesk@outlook.com>
2022-05-21 13:45:15 +0200

    Version 1.51 - Updated visitor count code

--- frontend/js/visitor.js
+++ frontend/js/visitor.js
@@ -2,5 +2,5 @@ fetch('https://c8bhagdr6l.execute-api.eu-central-1.amazonaws.com/PROD/put')
 /*    .then(() => fetch('https://c8bhagdr6l.execute-api.eu-central-1.amazonaws.com/PROD/get'))*/
     .then(response => response.json())
     .then(data => {
-        document.getElementById('visitorCount').innerText = data['counter']
+        document.getElementById('visitorCount').innerText = data['visitorCount']
     })
