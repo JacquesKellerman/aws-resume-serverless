aeede2fa6350efddb1b772a74d268f38d6cd96fa
Jacques Kellerman <jacquesk@outlook.com>
2022-05-24 19:55:33 +0200

    Version 1.8 - Added all CI/CD integration

---
+++ backend/get_function/test_handler.py
@@ -0,0 +1,12 @@
+import unittest
+
+from get_function.app import lambda_handler
+
+class TestAPI(unittest.TestCase):
+    def test_getApi_works(self):
+
+        result = lambda_handler(0,0)
+        self.assertEqual(result['statusCode'], 200)
+
+if __name__ == '__main__':
+    unittest.main()
--- backend/tests/unit/test_handler.py
+++ backend/tests/unit/test_handler.py
@@ -1,73 +1,55 @@
-import json
+### Unit test for the getVisitorCount Lambda function
 
-import pytest
+import boto3
+import os
+import unittest
+from moto import mock_dynamodb2
+from app.py import handler
 
-from hello_world import app
+def aws_setup():
+  # Mocked AWS Credentials for moto
+  os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+  os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+  os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+  os.environ['AWS_SESSION_TOKEN'] = 'testing'
+  os.environ['AWS_DEFAULT_REGION'] = 'eu-central-1'
 
+  # Database table name into env variable
+  os.environ['databaseName'] = 'testingdb'
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+class TestLambdaDDB(unittest.TestCase):
+  @mock_dynamodb2
+  def test_handler(self):
+    # Create dynamodb boto3 object
+    dynamodb = boto3.client('dynamodb')
+    # Get dynamodb table name from env
+    ddbTableName = os.environ['databaseName']
+    
+    # Create mock table
+    dynamodb.create_table(
+      TableName = ddbTableName,
+      BillingMode='PAY_PER_REQUEST',
+      AttributeDefinitions=[
+          {
+              'AttributeName': 'id',
+              'AttributeType': 'S'
+          },
+      ],
+      KeySchema=[
+          {
+              'AttributeName': 'id',
+              'KeyType': 'HASH'
+          },
+      ]
+    )
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+    # Print Lambda response
+    LambdaResponse = handler(0, 0)
+    print("Lambda response: ", LambdaResponse)
 
+    # Run unit test against Lambda status code
+    self.assertEqual(200, LambdaResponse['statusCode'])
 
-def test_lambda_handler(apigw_event, mocker):
-
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
-
-    assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+if __name__ == '__main__':
+  aws_setup()
+  unittest.main()
