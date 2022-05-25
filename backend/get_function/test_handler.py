614b17ad7ae6e8e1a860804816b0c6ac8f2fe311
Jacques Kellerman <jacquesk@outlook.com>
2022-05-25 15:07:23 +0200

    Moved tmate debugging step again.

--- backend/get_function/test_handler.py
+++ backend/get_function/test_handler.py
@@ -1,12 +1,55 @@
+### Unit test for the getVisitorCount Lambda function
+import boto3
+import os
 import unittest
 
-from app import lambda_handler
+from moto import mock_dynamodb2
+from app import handler
+
+def aws_setup():
+  # Mocked AWS Credentials for moto
+  os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+  os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+  os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+  os.environ['AWS_SESSION_TOKEN'] = 'testing'
+  os.environ['AWS_DEFAULT_REGION'] = 'eu-central-1'
+
+  # Database table name into env variable
+  os.environ['databaseName'] = 'testingdb'
 
 class TestAPI(unittest.TestCase):
-    def test_getApi_works(self):
+  @mock_dynamodb2
+  def test_handler(self):
+    # Create dynamodb boto3 object
+    dynamodb = boto3.client('dynamodb')
+    # Get dynamodb table name from env
+    ddbTableName = os.environ['databaseName']
+    
+    # Create mock table
+    dynamodb.create_table(
+      TableName = ddbTableName,
+      BillingMode='PAY_PER_REQUEST',
+      AttributeDefinitions=[
+          {
+              'AttributeName': 'id',
+              'AttributeType': 'S'
+          },
+      ],
+      KeySchema=[
+          {
+              'AttributeName': 'id',
+              'KeyType': 'HASH'
+          },
+      ]
+    )
 
-        result = lambda_handler(0,0)
-        self.assertEqual(result['statusCode'], 200)
+    # Print Lambda response
+    LambdaResponse = handler(0, 0)
+    print("Lambda response: ", LambdaResponse)
+
+    # Run unit test against Lambda status code
+    self.assertEqual(200, LambdaResponse['statusCode'])
 
 if __name__ == '__main__':
-    unittest.main()
+  aws_setup()
+  unittest.main()
